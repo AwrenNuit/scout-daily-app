93005e38ed7770b7064ebfc8ab044ad3286f85d8
AwrenNuit <a.rennaker@yahoo.com>
2020-01-10 14:46:37 -0600

    basic styling and layout

--- src/components/RegisterPage/RegisterPage.js
+++ src/components/RegisterPage/RegisterPage.js
@@ -1,7 +1,10 @@
 import React, { Component } from 'react';
 import {connect} from 'react-redux';
+import Button from '@material-ui/core/Button';
+import TextField from '@material-ui/core/TextField';
 
 class RegisterPage extends Component {
+
   state = {
     username: '',
     password: '',
@@ -41,47 +44,65 @@ class RegisterPage extends Component {
           </h2>
         )}
         <form onSubmit={this.registerUser}>
-          <h1>Register User</h1>
+          <h1>Register</h1>
           <div>
-            <label htmlFor="username">
-              Username:
-              <input
-                type="text"
+            <center>
+
+              <TextField
+                id="outlined-basic"
+                label="username"
+                variant="outlined"
                 name="username"
+                size="small"
                 value={this.state.username}
                 onChange={this.handleInputChangeFor('username')}
+                style={{width:"90%",marginBottom:"20px",backgroundColor:"white"}}
               />
-            </label>
+
+            </center>
           </div>
           <div>
-            <label htmlFor="password">
-              Password:
-              <input
-                type="password"
+            <center>
+
+              <TextField
+                id="outlined-basic"
+                label="password"
+                variant="outlined"
                 name="password"
+                type="password"
+                size="small"
                 value={this.state.password}
                 onChange={this.handleInputChangeFor('password')}
+                style={{width:"90%",marginBottom:"20px",backgroundColor:"white"}}
               />
-            </label>
+
+            </center>
           </div>
           <div>
-            <input
-              className="register"
-              type="submit"
-              name="submit"
-              value="Register"
-            />
+            <center>
+              <Button 
+                variant="contained" 
+                color="primary"
+                type="submit"
+                value="Register"
+                style={{width:"90%",marginBottom:"10px"}}
+              >
+                Register
+              </Button>
+            </center>
+          </div>
+          <hr />
+          <div>
+            <center>
+            <Button 
+              onClick={() => {this.props.dispatch({type: 'SET_TO_LOGIN_MODE'})}}
+              style={{textTransform:"lowercase",marginTop:"10px"}}
+            >
+              cancel
+            </Button>
+            </center>
           </div>
         </form>
-        <center>
-          <button
-            type="button"
-            className="link-button"
-            onClick={() => {this.props.dispatch({type: 'SET_TO_LOGIN_MODE'})}}
-          >
-            Login
-          </button>
-        </center>
       </div>
     );
   }
