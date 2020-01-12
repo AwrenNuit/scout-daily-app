3247f176387e8f11871c6bc10bd31bb4601adf90
AwrenNuit <a.rennaker@yahoo.com>
2020-01-12 14:19:07 -0600

    removed unused components

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -8,9 +8,7 @@ import {
 import {connect} from 'react-redux';
 
 import ProtectedRoute from '../ProtectedRoute/ProtectedRoute';
-import AboutPage from '../AboutPage/AboutPage';
 import Home from '../Home/Home';
-import InfoPage from '../InfoPage/InfoPage';
 import './App.css';
 
 class App extends Component {
@@ -25,13 +23,6 @@ class App extends Component {
           <Switch>
             {/* Visiting localhost:3000 will redirect to localhost:3000/home */}
             <Redirect exact from="/" to="/home" />
-            {/* Visiting localhost:3000/about will show the about page.
-            This is a route anyone can see, no login necessary */}
-            <Route
-              exact
-              path="/about"
-              component={AboutPage}
-            />
             {/* For protected routes, the view could show one of several things on the same route.
             Visiting localhost:3000/home will show the UserPage if the user is logged in.
             If the user is not logged in, the ProtectedRoute will show the 'Login' or 'Register' page.
@@ -40,13 +31,6 @@ class App extends Component {
               exact
               path="/home"
               component={Home}
-            />
-            {/* This works the same as the other protected route, except that if the user is logged in,
-            they will see the info page instead. */}
-            <ProtectedRoute
-              exact
-              path="/info"
-              component={InfoPage}
             />
             {/* If none of the other routes matched, we will show a 404. */}
             <Route render={() => <h1>404</h1>} />
