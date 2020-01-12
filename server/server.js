40ea92641757744005dc275898249aab7c1c8b57
AwrenNuit <a.rennaker@yahoo.com>
2020-01-12 14:34:37 -0600

    added image route

--- server/server.js
+++ server/server.js
@@ -10,6 +10,7 @@ const passport = require('./strategies/user.strategy');
 // Route includes
 const userRouter = require('./routes/user.router');
 const promptRouter = require('./routes/prompt.router');
+const imageRouter = require('./routes/image.router');
 
 // Body parser middleware
 app.use(bodyParser.json());
@@ -25,6 +26,7 @@ app.use(passport.session());
 /* Routes */
 app.use('/api/user', userRouter);
 app.use('/api/prompt', promptRouter);
+app.use('/api/image', imageRouter);
 
 // Serve static files
 app.use(express.static('build'));
