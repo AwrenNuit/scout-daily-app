71c979e08821b4d534f9f0ca06089b692c4a44cf
AwrenNuit <a.rennaker@yahoo.com>
2020-01-12 22:11:47 -0600

    created sga to get avatars of followed users

--- src/redux/sagas/getImageSaga.js
+++ src/redux/sagas/getImageSaga.js
@@ -4,11 +4,22 @@ import {takeLatest, put} from 'redux-saga/effects';
 // GET all images
 function* getAllImage(action){
   try{
-      const getResponse = yield axios.get(`/api/image`, action.payload);
+      const getResponse = yield axios.get(`/api/image/all`, action.payload);
       yield put({type: `SET_ALL_IMAGE`, payload: getResponse.data});
   }
   catch(error){
       console.log('error in GET all images', error);
+  }
+}
+
+// GET images of followed users
+function* getFollowedAvatar(action){
+  try{
+      const getResponse = yield axios.get(`/api/image/following`, action.payload);
+      yield put({type: `SET_FOLLOWING`, payload: getResponse.data});
+  }
+  catch(error){
+      console.log('error in GET followed user avatars', error);
   }
 }
 
@@ -36,6 +47,7 @@ function* postImage(action){
 
 function* imageSaga() {
   yield takeLatest('GET_ALL_IMAGE', getAllImage);
+  yield takeLatest('GET_FOLLOWING', getFollowedAvatar);
   yield takeLatest('GET_THIS_IMAGE', getThisImage);
   yield takeLatest('POST_IMAGE', postImage);  
 }
