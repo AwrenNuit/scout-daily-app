a844217b3d2427086919f421632c97d4805de141
AwrenNuit <a.rennaker@yahoo.com>
2020-01-15 16:55:48 -0600

    moved NavBar placement

--- src/components/Following/Following.js
+++ src/components/Following/Following.js
@@ -12,13 +12,13 @@ class Following extends Component{
   render(){
     return(
       <>
-          {this.props.reduxState.map(image =>
-            <div>
-              <img className="avatar" src={"https://scout-daily.s3.us-east-2.amazonaws.com/"+image.image_url} alt={image.username} />
-              <div>{image.username}</div>
-              <NavBar />
-            </div>
-          )}
+        {this.props.reduxState.map(image =>
+          <div>
+            <img className="avatar" src={"https://scout-daily.s3.us-east-2.amazonaws.com/"+image.image_url} alt={image.username} />
+            <div>{image.username}</div>
+          </div>
+        )}
+        <NavBar />
       </>
     );
   }
