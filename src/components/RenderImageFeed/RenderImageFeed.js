9debf71dbfe4c55b641380b2c6062793fa409042
AwrenNuit <a.rennaker@yahoo.com>
2020-01-15 19:36:23 -0600

    added Link to view image

--- src/components/RenderImageFeed/RenderImageFeed.js
+++ src/components/RenderImageFeed/RenderImageFeed.js
@@ -1,4 +1,5 @@
 import React, {Component} from 'react';
+import { Link } from "react-router-dom";
 import {connect} from 'react-redux';
 
 class RenderAllImage extends Component{
@@ -13,7 +14,9 @@ class RenderAllImage extends Component{
       <>
         {this.props.reduxState ? this.props.reduxState.map(image=>
             <span key={image.id}>
-              <div className="all-img" style={{backgroundImage:`url(https://scout-daily.s3.us-east-2.amazonaws.com/${image.image_url})`}}></div>
+              <Link to={"/view-photo/"+image.id}>
+                <div className="all-img" style={{backgroundImage:`url(https://scout-daily.s3.us-east-2.amazonaws.com/${image.image_url})`}}></div>
+              </Link>
             </span>
           ) : <p>Follow someone</p>
         }
