078d8a5818ace954e074e3bbb8f44dd2ecf13476
AwrenNuit <a.rennaker@yahoo.com>
2020-01-18 17:36:55 -0600

    added styling

--- src/components/ViewThisImage/ViewThisImage.js
+++ src/components/ViewThisImage/ViewThisImage.js
@@ -1,5 +1,6 @@
 import React, {Component} from 'react';
 import {connect} from 'react-redux';
+import { Link } from "react-router-dom";
 import './ViewThisImage.css';
 import FavoriteBorderIcon from '@material-ui/icons/FavoriteBorder';
 import NavBar from '../NavBar/NavBar';
@@ -17,17 +18,30 @@ class ViewThisImage extends Component{
   render(){
     return(
       <>
-        <center>
+        <div className="view-card">
           <div>
-            <div>
-              <img className="view-img" src={this.props.reduxState.image_url} alt={this.props.reduxState.caption} />
-            </div>
-            <div>{this.props.reduxState.caption}</div>
-            <FavoriteBorderIcon onClick={()=>this.handleLike(this.props.reduxState.id)} />
-            {this.props.reduxState.likes} likes
+            <Link to={"/profile/"+this.props.reduxState.user_id}>
+              <img className="view-avatar" src={this.props.reduxState.avatar} alt={this.props.reduxState.username} />
+              <span className="view-username">{this.props.reduxState.username}</span>
+            </Link>
           </div>
-            <div>COMMENTS GO HERE</div>
-        </center>
+          <span>
+            <center>
+              <img className="view-img" src={this.props.reduxState.image_url} alt={this.props.reduxState.caption} />
+            </center>
+            <div>
+              <FavoriteBorderIcon 
+                onClick={()=>this.handleLike(this.props.reduxState.id)} 
+                style={{marginLeft:"40px",cursor:"pointer"}}
+              />
+              <span className="view-likes">{this.props.reduxState.likes} likes</span>
+            </div>
+            <center>
+              <div className="view-caption">{this.props.reduxState.caption}</div>
+            </center>
+          </span>
+        </div>
+        <div>COMMENTS GO HERE</div>
         <NavBar />
       </>
     );
