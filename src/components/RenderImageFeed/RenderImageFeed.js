fe5f0ac7c5a39f2b41c4920f6635101ed5a24278
AwrenNuit <a.rennaker@yahoo.com>
2020-01-18 13:27:10 -0600

    added like button

--- src/components/RenderImageFeed/RenderImageFeed.js
+++ src/components/RenderImageFeed/RenderImageFeed.js
@@ -2,11 +2,17 @@ import React, {Component} from 'react';
 import { Link } from "react-router-dom";
 import {connect} from 'react-redux';
 import './RenderImageFeed.css';
+import FavoriteBorderIcon from '@material-ui/icons/FavoriteBorder';
 
 class RenderImageFeed extends Component{
 
   // GET images to render
   componentDidMount(){
+    this.props.dispatch({type: `GET_IMAGE_FEED`});
+  }
+
+  handleLike = (id) => {
+    this.props.dispatch({type: `ADD_LIKE`, payload: id});
     this.props.dispatch({type: `GET_IMAGE_FEED`});
   }
 
@@ -20,6 +26,10 @@ class RenderImageFeed extends Component{
                 <Link to={"/view-image/"+image.id}>
                   <img className="feed-img" src={image.image_url} alt={image.caption} />
                 </Link>
+                <div>
+                  <FavoriteBorderIcon onClick={()=>this.handleLike(image.id)} />
+                  <span>{image.likes} likes</span>
+                </div>
                 <div className="feed-caption">{image.caption}</div>
               </span>
             </center>
