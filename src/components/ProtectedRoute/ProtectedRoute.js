cd813fc0bb819aae915fd67b7ce6a75b3384fbb2
AwrenNuit <a.rennaker@yahoo.com>
2020-01-24 08:24:17 -0600

    edited comments, updated pool for Heroku deployment

--- src/components/ProtectedRoute/ProtectedRoute.js
+++ src/components/ProtectedRoute/ProtectedRoute.js
@@ -4,19 +4,8 @@ import {connect} from 'react-redux';
 import LoginPage from '../LoginPage/LoginPage';
 import RegisterPage from '../RegisterPage/RegisterPage';
 
-// A Custom Wrapper Component -- This will keep our code DRY.
-// Responsible for watching redux state, and returning an appropriate component
-// API for this component is the same as a regular route
-
-// THIS IS NOT SECURITY! That must be done on the server
-// A malicious user could change the code and see any view
-// so your server-side route must implement real security
-// by checking req.isAuthenticated for authentication
-// and by checking req.user for authorization
-
 const ProtectedRoute = (props) => {
-  // Using destructuring, this takes ComponentToProtect from component
-  // prop and grabs all other props to pass them along to Route
+
   const {
     // Alias prop 'component' as 'ComponentToProtect'
     component: ComponentToProtect,
@@ -41,21 +30,14 @@ const ProtectedRoute = (props) => {
     ComponentToShow = RegisterPage;
   }
 
-  // We return a Route component that gets added to our list of routes
   return (
       <Route
-        // all props like 'exact' and 'path' that were passed in
-        // are now passed along to the 'Route' Component
         {...otherProps}
         component={ComponentToShow}
       />
   )
 }
 
-// Instead of taking everything from state, we just want the user and loginMode
-// to determine which page we should show the user
-// if you wanted you could write this code like this:
-// const mapStateToProps = ({ user, loginMode }) => ({ user, loginMode });
 const mapStateToProps = (state) => {
   return {
     user: state.user,
