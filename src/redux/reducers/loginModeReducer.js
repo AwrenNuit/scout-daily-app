b4a3e402c8595ea297057be77307ae69de5c2c58
AwrenNuit <a.rennaker@yahoo.com>
2020-02-09 17:08:37 -0600

    minor formatting updates

--- src/redux/reducers/loginModeReducer.js
+++ src/redux/reducers/loginModeReducer.js
@@ -1,13 +1,12 @@
 const loginMode = (state = 'login', action) => {
-    switch (action.type) {
-      case 'SET_TO_LOGIN_MODE':
-        return 'login';
-      case 'SET_TO_REGISTER_MODE':
-        return 'register';
-      default:
-        return state;
-    }
-  };
+  switch (action.type) {
+    case 'SET_TO_LOGIN_MODE':
+      return 'login';
+    case 'SET_TO_REGISTER_MODE':
+      return 'register';
+    default:
+      return state;
+  }
+};
 
-  export default loginMode;
-  
+export default loginMode;
