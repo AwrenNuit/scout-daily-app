b164dbddf14b3559f80e4a7995486c650a3f6e3d
AwrenNuit <a.rennaker@yahoo.com>
2020-02-09 16:10:24 -0600

    slight formatting adjustments

--- server/server.js
+++ server/server.js
@@ -4,43 +4,39 @@ const app = express();
 const bodyParser = require('body-parser');
 const sessionMiddleware = require('./modules/session-middleware');
 const passport = require('./strategies/user.strategy');
+const PORT = process.env.PORT || 5000;
 
 // Route includes
-const userRouter = require('./routes/user.router');
-const promptRouter = require('./routes/prompt.router');
-const imageRouter = require('./routes/image.router');
 const avatarRouter = require('./routes/avatar.route');
-const likeRouter = require('./routes/like.router');
 const commentRouter = require('./routes/comment.router');
 const followingRouter = require('./routes/following.router');
+const imageRouter = require('./routes/image.router');
+const likeRouter = require('./routes/like.router');
+const promptRouter = require('./routes/prompt.router');
+const userRouter = require('./routes/user.router');
 
 // Body parser middleware
 app.use(bodyParser.json({limit: '50mb'}));
 app.use(bodyParser.urlencoded({limit: '50mb', extended: true}));
 
-// Passport Session Configuration //
+// Passport Session Configuration
 app.use(sessionMiddleware);
 
-// start up passport sessions
+// Start up passport sessions
 app.use(passport.initialize());
 app.use(passport.session());
 
-/* Routes */
-app.use('/api/user', userRouter);
-app.use('/api/prompt', promptRouter);
-app.use('/api/image', imageRouter);
+// Routes
 app.use('/api/avatar', avatarRouter);
-app.use('/api/like', likeRouter);
 app.use('/api/comment', commentRouter);
 app.use('/api/following', followingRouter);
+app.use('/api/image', imageRouter);
+app.use('/api/like', likeRouter);
+app.use('/api/prompt', promptRouter);
+app.use('/api/user', userRouter);
 
 // Serve static files
 app.use(express.static('build'));
 
-// App Set //
-const PORT = process.env.PORT || 5000;
-
-/** Listen * */
-app.listen(PORT, () => {
-  console.log(`Listening on port: ${PORT}`);
-});
+// Listen
+app.listen(PORT, () => console.log(`Listening on port: ${PORT}`));
