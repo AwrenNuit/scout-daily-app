aac41404de0abe4e57be9eeb4e26b849d525fc79
AwrenNuit <a.rennaker@yahoo.com>
2020-02-09 17:07:53 -0600

    minor comment clarifications

--- src/redux/reducers/index.js
+++ src/redux/reducers/index.js
@@ -11,16 +11,16 @@ import searchResult from './searchResultReducer';
 import user from './userReducer';
 
 const rootReducer = combineReducers({
-  comment, // stores image comments
-  dailyPrompt, // stores daily photography prompt
-  details, // stores user profile details
-  errors, // contains registrationMessage and loginMessage
-  following, // stores followed user avatars and details
-  image, // stores images
-  like, // stores individual likes
-  loginMode, // will have a value of 'login' or 'registration' to control which screen is shown
-  searchResult, // stores search results
-  user, // will have an id and username if someone is logged in
+  comment, // Stores image comments
+  dailyPrompt, // Stores daily photography prompt
+  details, // Stores user profile details
+  errors, // Stores registrationMessage and loginMessage
+  following, // Stores followed user avatars and details
+  image, // Stores images
+  like, // Stores individual likes
+  loginMode, // Will have a value of 'login' or 'registration' to control which screen is shown
+  searchResult, // Stores search results
+  user, // Will have an id and username if someone is logged in
 });
 
 export default rootReducer;
