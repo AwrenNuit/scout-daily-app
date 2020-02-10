5d5599198cf979526b26845d235c87f999f3cf7b
AwrenNuit <a.rennaker@yahoo.com>
2020-02-09 21:30:20 -0600

    converted component to use hooks

--- src/components/RenderOtherUserImage/RenderOtherUserImage.js
+++ src/components/RenderOtherUserImage/RenderOtherUserImage.js
@@ -1,36 +1,32 @@
-import React, {Component} from 'react';
-import {connect} from 'react-redux';
-import './RenderOtherUserImage.css';
+import React, {useEffect} from 'react';
 import {Link} from 'react-router-dom';
+import {useDispatch, useSelector} from 'react-redux';
+import './RenderOtherUserImage.css';
 
-class RenderUserImage extends Component{
+export default function RenderUserImage(props) {
 
-  componentDidMount(){
-    this.props.dispatch({type: `GET_ALL_OTHER_USER_IMAGE`, payload: this.props.id});
-  }
+  const dispatch = useDispatch();
+  const image = useSelector(state => state.image.allOtherUserImage);
 
-  render(){
-    return(
-      <div>
-        {this.props.reduxState ? 
-          this.props.reduxState.map(image=>
-            <span key={image.id}>
-              <Link to={"/view-image/"+image.id}>
-                <img className="other-user-img" src={image.image_url} alt={image.description} />
-              </Link>
-            </span>
-          )
-          :
-          <p>No posts yet</p>
-        }
-        <div className="bottom-whitespace"></div>
-      </div>
-    );
-  }
+  // Run on component mount
+  useEffect(()=>{
+    dispatch({type: `GET_ALL_OTHER_USER_IMAGE`, payload: props.id});
+  }, []);
+
+  return(
+    <div>
+      {image ? 
+        image.map(image=>
+          <span key={image.id}>
+            <Link to={"/view-image/"+image.id}>
+              <img className="other-user-img" src={image.image_url} alt={image.description} />
+            </Link>
+          </span>
+        )
+        :
+        <p>No posts yet</p>
+      }
+      <div className="bottom-whitespace"></div>
+    </div>
+  );
 }
-
-const putReduxStateOnProps = (reduxState)=>({
-  reduxState: reduxState.image.allOtherUserImage
-});
-
-export default connect(putReduxStateOnProps)(RenderUserImage);
