3b705bc9cdf5f70c546889ca700afd16e88d8207
AwrenNuit <a.rennaker@yahoo.com>
2020-02-10 08:33:33 -0600

    converted component to use hooks

--- src/components/Following/Following.js
+++ src/components/Following/Following.js
@@ -1,44 +1,47 @@
-import React, {Component} from 'react';
-import {connect} from 'react-redux';
+import React, {useEffect} from 'react';
+import {useHistory} from 'react-router-dom';
+import {useDispatch, useSelector} from 'react-redux';
 import './Following.css';
 import NavBar from '../NavBar/NavBar';
 
-class Following extends Component{
+export default function Following() {
 
-  componentDidMount(){
-    this.props.dispatch({type: `GET_FOLLOWING`});
-  }
+  const dispatch = useDispatch();
+  const history = useHistory();
+  const avatar = useSelector(state => state.following.followingAvatar);
+
+  // Run on component mount
+  useEffect(()=>{
+    dispatch({type: `GET_FOLLOWING`});
+  }, []);
 
   // View selected user's profile
-  handleClick = (id) => {
-    this.props.history.push(`/profile/${id}`);
+  const handleClick = id => {
+    history.push(`/profile/${id}`);
   }
 
-  render(){
-    return(
-      <>
-        <h2 className="heading-2">Following</h2>
-        {this.props.reduxState ?
-          <div className="following-main-flex" >
-            {this.props.reduxState.map(image =>
-              <div className="following-col" key={image.id}>
-                <img className="following-avatar" onClick={()=>this.handleClick(image.id)} src={image.avatar} alt={image.username} />
-                <div>{image.username}</div>
-              </div>
-            )}
-          </div>
-          :
-          <p>Not following anyone</p>
-        }
-        <div className="bottom-whitespace"></div>
-        <NavBar />
-      </>
-    );
-  }
+  return(
+    <>
+      <h2 className="heading-2">Following</h2>
+      {avatar ?
+        <div className="following-main-flex" >
+          {avatar.map(image =>
+            <div className="following-col" key={image.id}>
+              <img 
+                className="following-avatar" 
+                onClick={()=>handleClick(image.id)} 
+                src={image.avatar} 
+                alt={image.username} 
+              />
+              <div>{image.username}</div>
+            </div>
+          )}
+        </div>
+        :
+        <p>Not following anyone</p>
+      }
+      <div className="bottom-whitespace"></div>
+      <NavBar />
+    </>
+  );
 }
-
-const putReduxStateOnProps = (reduxState)=>({
-  reduxState: reduxState.following.followingAvatar
-});
-
-export default connect(putReduxStateOnProps)(Following);
