790cdeba8018e5730b161e32b545f23de19a4181
AwrenNuit <a.rennaker@yahoo.com>
2020-02-10 08:22:39 -0600

    converted component to use hooks

--- src/components/RenderImageFeed/RenderImageFeed.js
+++ src/components/RenderImageFeed/RenderImageFeed.js
@@ -1,34 +1,32 @@
-import React, {Component} from 'react';
-import { Link } from "react-router-dom";
+import React from 'react';
+import {Link} from "react-router-dom";
 import './RenderImageFeed.css';
 import LikeFeedImage from '../LikeFeedImage/LikeFeedImage';
 
-class RenderImageFeed extends Component{
+export default function RenderImageFeed(props) {
 
-  render(){
-    return(
-      <div className="feed-card first-card">
-        <div>
-          <Link to={"/profile/"+this.props.image.user_id}>
-            <img className="feed-avatar" src={this.props.image.avatar} alt={this.props.image.username} />
-            <span className="feed-username">{this.props.image.username}</span>
-          </Link>
-        </div>
-        <span>
-          <center>
-            <Link to={"/view-image/"+this.props.image.id}>
-              <img className="feed-img" src={this.props.image.image_url} alt={this.props.image.caption} />
-            </Link>
-          </center>
-          <div>
-          <LikeFeedImage image={this.props.image} />
-            <span className="feed-likes">{this.props.image.likes} likes</span>
-          </div>
-          <div className="feed-caption">{this.props.image.caption}</div>
-        </span>
+  const image = props.image;
+
+  return(
+    <div className="feed-card first-card">
+      <div>
+        <Link to={"/profile/"+image.user_id}>
+          <img className="feed-avatar" src={image.avatar} alt={image.username} />
+          <span className="feed-username">{image.username}</span>
+        </Link>
       </div>
-    );
-  }
+      <span>
+        <center>
+          <Link to={"/view-image/"+image.id}>
+            <img className="feed-img" src={image.image_url} alt={image.caption} />
+          </Link>
+        </center>
+        <div>
+        <LikeFeedImage image={image} />
+          <span className="feed-likes">{image.likes} likes</span>
+        </div>
+        <div className="feed-caption">{image.caption}</div>
+      </span>
+    </div>
+  );
 }
-
-export default RenderImageFeed;
