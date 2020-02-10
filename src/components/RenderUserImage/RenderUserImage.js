0980b9b82de1d475efc9d926bfca3eebd887ef22
AwrenNuit <a.rennaker@yahoo.com>
2020-02-09 21:24:23 -0600

    converted component to use hooks

--- src/components/RenderUserImage/RenderUserImage.js
+++ src/components/RenderUserImage/RenderUserImage.js
@@ -1,35 +1,32 @@
-import React, {Component} from 'react';
-import {connect} from 'react-redux';
-import './RenderUserImage.css';
+import React, {useEffect} from 'react';
 import {Link} from 'react-router-dom';
+import {useDispatch, useSelector} from 'react-redux';
+import './RenderUserImage.css';
 
-class RenderUserImage extends Component{
+export default function RenderUserImage() {
 
-  componentDidMount(){
-    this.props.dispatch({type: `GET_ALL_USER_IMAGE`});
-  }
+  const dispatch = useDispatch();
+  const image = useSelector(state => state.image.allUserImage);
 
-  render(){
-    return(
-      <div>
-        {this.props.reduxState ? this.props.reduxState.map(image=>
-            <span key={image.id}>
-              <Link to={"/edit-photo/"+image.id}>
-                <img className="user-img" src={image.image_url} alt={image.description} />
-              </Link>
-            </span>
-          )
-          :
-          <p>Add some pics! :)</p>
-        }
-        <div className="bottom-whitespace"></div>
-      </div>
-    );
-  }
+  // Run on component mount
+  useEffect(()=>{
+    dispatch({type: `GET_ALL_USER_IMAGE`});
+  }, []);
+
+  return(
+    <div>
+      {image ? 
+        image.map(image =>
+          <span key={image.id}>
+            <Link to={"/edit-photo/"+image.id}>
+              <img className="user-img" src={image.image_url} alt={image.description} />
+            </Link>
+          </span>
+        )
+        :
+        <p>Add some pics! :)</p>
+      }
+      <div className="bottom-whitespace"></div>
+    </div>
+  );
 }
-
-const putReduxStateOnProps = (reduxState)=>({
-  reduxState: reduxState.image.allUserImage
-});
-
-export default connect(putReduxStateOnProps)(RenderUserImage);
