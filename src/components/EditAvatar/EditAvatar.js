8359dcbf0ab5d278344bf49e4a2d5d8fe3126d86
AwrenNuit <a.rennaker@yahoo.com>
2020-02-12 08:59:08 -0600

    converted component to use hooks

--- src/components/EditAvatar/EditAvatar.js
+++ src/components/EditAvatar/EditAvatar.js
@@ -1,124 +1,110 @@
-import React, {Component} from 'react';
-import {connect} from 'react-redux';
+import React, {useCallback, useEffect, useState} from 'react';
+import {useDispatch, useSelector} from 'react-redux';
+import { useHistory } from 'react-router-dom';
 import './EditAvatar.css';
 import AvatarEditor from 'react-avatar-editor';
 import NavBar from '../NavBar/NavBar';
 import Button from '@material-ui/core/Button';
 
-class EditAvatar extends Component{
+export default function EditAvatar() {
 
-  state = {
-    file: '',
-    scale: 1,
-  }
+  const dispatch = useCallback(useDispatch());
+  const history = useHistory();
+  const [editor, setEditor] = useState('');
+  const [file, setFile] = useState('');
+  const [scale, setScale] = useState(1);
+  const userDetails = useSelector(state => state.details.userDetails);
 
-  componentDidMount(){
-    this.props.dispatch({type: `GET_USER_DETAILS`});
-  }
-  
-  componentDidUpdate(prevProps){
-    if(this.props.reduxState !== prevProps.reduxState){
-      this.setState({file: this.props.reduxState.avatar});
-    }
-  }
+  // Run on component mount
+  useEffect(()=>{
+    dispatch({type: `GET_USER_DETAILS`});
+  }, []);
+
+  // Set file on update
+  useEffect(()=>{
+    setFile(userDetails.avatar);
+  }, [setFile]);
 
   // Get image from canvas
-  handleCanvas = () => {
-    const canvasScaled = this.editor.getImageScaledToCanvas().toDataURL('image/png');
-    this.props.dispatch({type: `UPDATE_AVATAR`, payload: canvasScaled});
-  }
-
-  // Update selected file in local state
-  handleFileChange = (e) => {
-    this.setState({file: URL.createObjectURL(e.target.files[0])});
+  const handleCanvas = () => {
+    const canvasScaled = editor.getImageScaledToCanvas().toDataURL('image/png');
+    dispatch({type: `UPDATE_AVATAR`, payload: canvasScaled});
   }
 
   // Set scale of image zoom
-  handleZoom = (e) => {
+  const handleZoom = e => {
     let scale = parseFloat(e.target.value);
-    this.setState({
-      scale: scale
-    });
+    setScale(scale);
   }
 
   // Save the image from canvas into database
-  onClickSave = () => {
-    if (this.editor) {
-      this.handleCanvas();
-      this.pushHistory();
+  const onClickSave = () => {
+    if(editor) {
+      handleCanvas();
+      pushHistory();
     }
   }
 
   // Return to user's profile
-  pushHistory = () => {
-    this.props.history.push('/profile');
-  }
+  const pushHistory = () => history.push('/profile');
 
   // Set editor for canvas grab
-  setEditorRef = (editor) => this.editor = editor;
+  const setEditorRef = edit => setEditor(edit);
 
-  render(){
-    return(
-      <center>
-        <div>
-          {this.state.file ?
-            <>
-            <div className="avatar-editor-div">
-                <AvatarEditor
-                  ref={this.setEditorRef}
-                  image={this.state.file}
-                  width={250}
-                  height={250}
-                  border={50}
-                  borderRadius={125}
-                  color={[0, 0, 0, 0.8]} // RGBA
-                  scale={this.state.scale}
-                  rotate={0}
+  return(
+    <center>
+      <div>
+        {file ?
+          <>
+          <div className="avatar-editor-div">
+              <AvatarEditor
+                ref={setEditorRef}
+                image={file}
+                width={250}
+                height={250}
+                border={50}
+                borderRadius={125}
+                color={[0, 0, 0, 0.8]} // RGBA
+                scale={scale}
+                rotate={0}
+              />
+              <div>
+                <span>Zoom:</span> 
+                <input 
+                  type="range" 
+                  step="0.1" 
+                  min="1" 
+                  max="2" 
+                  name="scale" 
+                  value={scale} 
+                  onChange={handleZoom} 
                 />
-                <div>
-                  <span>Zoom:</span> 
-                  <input 
-                    type="range" 
-                    step="0.1" 
-                    min="1" 
-                    max="2" 
-                    name="scale" 
-                    value={this.state.scale} 
-                    onChange={this.handleZoom} 
-                  />
-                </div>
               </div>
-            </>
-            :
-            <div className="avatar-whitespace"></div>
-          }
-          <div>
-            <label htmlFor="avatar-upload" className="custom-avatar-upload">
-              <p className="avatar-browse-btn-txt">BROWSE</p>
-            </label>
-            <input id="avatar-upload" type="file" onChange={this.handleFileChange} />
-          </div>
-        </div>
+            </div>
+          </>
+          :
+          <div className="avatar-whitespace"></div>
+        }
         <div>
-          <Button 
-            variant="contained" 
-            color="primary"
-            type="submit"
-            value="Save"
-            onClick={this.onClickSave}
-            style={{width:"80%",marginBottom:"50px",backgroundColor:"#bc75ff"}}
-          >
-            Apply
-          </Button>
+          <label htmlFor="avatar-upload" className="custom-avatar-upload">
+            <p className="avatar-browse-btn-txt">BROWSE</p>
+          </label>
+          <input id="avatar-upload" type="file" onChange={(e)=>setFile(URL.createObjectURL(e.target.files[0]))} /> 
         </div>
-        <NavBar history={this.props.history.location.pathname} />
-      </center>
-    );
-  }
+      </div>
+      <div>
+        <Button 
+          variant="contained" 
+          color="primary"
+          type="submit"
+          value="Save"
+          onClick={onClickSave}
+          style={{width:"80%",marginBottom:"50px",backgroundColor:"#bc75ff"}}
+        >
+          Apply
+        </Button>
+      </div>
+      <NavBar history={history.location.pathname} />
+    </center>
+  );
 }
-
-const putReduxStateOnProps = (reduxState)=>({
-  reduxState: reduxState.details.userDetails
-});
-
-export default connect(putReduxStateOnProps)(EditAvatar);
