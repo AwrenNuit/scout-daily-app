e85b9e1b0a7d6a0fa25bd965d0aa5b767e34f494
AwrenNuit <a.rennaker@yahoo.com>
2020-02-12 08:41:36 -0600

    converted component to use hooks

--- src/components/Comment/Comment.js
+++ src/components/Comment/Comment.js
@@ -1,136 +1,79 @@
-import React, {Component} from 'react';
-import {connect} from 'react-redux';
-import PropTypes from 'prop-types';
-import { withStyles } from '@material-ui/core/styles';
+import React, {useState, useEffect, useCallback} from 'react';
 import './Comment.css';
 import { TextField, Button } from '@material-ui/core';
 import ClearIcon from '@material-ui/icons/Clear';
+import { useDispatch, useSelector } from 'react-redux';
 
-const styles = ({
-  cssLabel: {
-    '&$cssFocused': {
-      color: '#bc75ff',
-    }
-  },
-  cssOutlinedInput: {
-    '&$cssFocused $notchedOutline': {
-      borderWidth: '3px',
-      borderColor: `#bc75ff !important`,
-    }
-  },
-  cssFocused: {},
-  notchedOutline: {},
-});
+export default function Comment(props) {
 
-class Comment extends Component{
+  const [comment, setComment] = useState('');
+  const dispatch = useCallback(useDispatch());
+  const existingComment = useSelector(state => state.comment);
+  const user = useSelector(state => state.user);
 
-  state = {
-    comment: ''
-  }
-
-  componentDidMount(){
-    this.props.dispatch({type: `VIEW_THIS_IMAGE`, payload: this.props.params_id});
-  }
+  // Run on component mount
+  useEffect(()=>{
+    dispatch({type: `VIEW_THIS_IMAGE`, payload: props.params_id});
+  }, []);
 
   // Dispatch comment to saga
-  dispatchComment = () => {
-    this.props.dispatch({type: `POST_COMMENT`, payload: {id: this.props.params_id, comment: this.state.comment}});
-  }
-
-  // Update local state with input value
-  handleCommentChange = (e) => {
-    this.setState({comment: e.target.value});
-  }
+  const dispatchComment = () => dispatch({type: `POST_COMMENT`, payload: {id: props.params_id, comment: comment}});
 
   // Delete this comment
-  handleDelete = (comment_id, image_id) => {
+  const handleDelete = (comment_id, image_id) => {
     let popup = window.confirm(`Delete this comment?`);
     if(popup){
-      this.props.dispatch({type: `DELETE_COMMENT`, payload: {comment_id: comment_id, image_id: image_id}});
+      dispatch({type: `DELETE_COMMENT`, payload: {comment_id: comment_id, image_id: image_id}});
     }
   }
 
   // Dispatch comment, reset local state
-  postComment = () => {
-    this.dispatchComment();
-    this.resetState();
+  const postComment = () => {
+    dispatchComment();
+    setComment('');
   }
 
-  // Clear local state
-  resetState = () => {
-    this.setState({comment: ''});
-  }
-
-  render(){
-    const { classes } = this.props;
-
-    return(
-      <>
-        {this.props.reduxState.map(comment =>
-          <div key={comment.id}>
-            <img className="comment-avatar" src={comment.avatar} alt={comment.username} />
-            <span className="comment-username">{comment.username}</span>
-            {this.props.user.username === comment.username ?
-                <ClearIcon 
-                  onClick={()=>this.handleDelete(comment.id, comment.image_id)}
-                  style={{position:"relative",top:"-45px",left:"175px",height:"20px",width:"40px",cursor:"pointer"}}
-                />
-              :
-              ''
-            }
-            <div className="comment-comment">{comment.comment}</div>
-            <hr className="comment-hr" />
-          </div>
-        )}
-        {this.props.user.id ?
-          <div className="comment-center-input">
-            <TextField
-              id="outlined-basic" 
-              label="add comment" 
-              variant="outlined"
-              onChange={this.handleCommentChange} 
-              value={this.state.comment}
-              multiline 
-              InputLabelProps={{
-                classes: {
-                  root: classes.cssLabel,
-                  focused: classes.cssFocused,
-                }
-              }}
-              InputProps={{
-                classes: {
-                  root: classes.cssOutlinedInput,
-                  focused: classes.cssFocused,
-                  notchedOutline: classes.notchedOutline,
-                }
-              }}
-              style={{width:"70%",marginBottom:"20px",marginTop:"10px"}}
-            />
-            <Button
-              variant="contained" 
-              color="primary"
-              onClick={this.postComment}
-              style={{position:"relative",height:"53px",width:"20%",backgroundColor:"#bc75ff",marginTop:"12px"}}
-            >
-              POST
-            </Button>
-          </div>
-          :
-          ''
-        }
-        
-      </>
-    );
-  }
+  return(
+    <>
+      {existingComment.map(comment =>
+        <div key={comment.id}>
+          <img className="comment-avatar" src={comment.avatar} alt={comment.username} />
+          <span className="comment-username">{comment.username}</span>
+          {user.username === comment.username ?
+              <ClearIcon 
+                onClick={()=>handleDelete(comment.id, comment.image_id)}
+                style={{position:"relative",top:"-45px",left:"175px",height:"20px",width:"40px",cursor:"pointer"}}
+              />
+            :
+            ''
+          }
+          <div className="comment-comment">{comment.comment}</div>
+          <hr className="comment-hr" />
+        </div>
+      )}
+      {user.id ?
+        <div className="comment-center-input">
+          <TextField
+            id="outlined-basic" 
+            label="add comment" 
+            variant="outlined"
+            onChange={(e)=>setComment(e.target.value)} 
+            value={comment}
+            multiline 
+            style={{width:"70%",marginBottom:"20px",marginTop:"10px"}}
+          />
+          <Button
+            variant="contained" 
+            color="primary"
+            onClick={postComment}
+            style={{position:"relative",height:"53px",width:"20%",backgroundColor:"#bc75ff",marginTop:"12px"}}
+          >
+            POST
+          </Button>
+        </div>
+        :
+        ''
+      }
+    </>
+  );
 }
-
-const putReduxStateOnProps = (reduxState)=>({
-  reduxState: reduxState.comment,
-  user: reduxState.user
-});
-
-Comment.propTypes = {
-  classes: PropTypes.object.isRequired,
-};
-
-export default withStyles(styles)(connect(putReduxStateOnProps)(Comment));
