67e64033b39c5fc8943344e24b946ddb023dbbbb
AwrenNuit <a.rennaker@yahoo.com>
2020-02-17 08:44:56 -0600

    converted comonent to use hooks

--- src/components/EditThisImage/EditThisImage.js
+++ src/components/EditThisImage/EditThisImage.js
@@ -1,146 +1,95 @@
-import React, {Component} from 'react';
-import {connect} from 'react-redux';
+import React, {useCallback, useEffect, useState} from 'react';
+import {useDispatch, useSelector} from 'react-redux';
+import { useParams, useHistory } from 'react-router-dom';
 import './EditThisImage.css';
-import PropTypes from 'prop-types';
-import { withStyles } from '@material-ui/core/styles';
 import Button from '@material-ui/core/Button';
 import TextField from '@material-ui/core/TextField';
 import Fab from '@material-ui/core/Fab';
 import DeleteForeverIcon from '@material-ui/icons/DeleteForever';
 import NavBar from '../NavBar/NavBar';
 
-const styles = ({
-  cssLabel: {
-    '&$cssFocused': {
-      color: '#bc75ff',
+export default function EditThisImage() {
+
+  const dispatch = useCallback(useDispatch());
+  const history = useHistory();
+  const match = useParams();
+  const image = useSelector(state => state.image.thisImage);
+  const [caption, setCaption] = useState('');
+  const [id, setId] = useState('');
+
+  // Run on component mount
+  useEffect(()=>{
+    dispatch({type: `GET_THIS_IMAGE`, payload: match.id});
+  }, [dispatch]);
+
+  // Run when image caption or id change
+  useEffect(()=>{
+    if(image){
+    setCaption(image.caption);
+    setId(image.id);
     }
-  },
-  cssOutlinedInput: {
-    '&$cssFocused $notchedOutline': {
-      borderWidth: '3px',
-      borderColor: `#bc75ff !important`,
-    }
-  },
-  cssFocused: {},
-  notchedOutline: {},
-});
-
-class EditThisImage extends Component{
-
-  state = {
-    caption: '',
-    id: ''
-  }
-
-  componentDidMount(){
-    this.props.dispatch({type: `GET_THIS_IMAGE`, payload: this.props.match.params.id});
-  }
-
-  componentDidUpdate(prevProps){
-    if(this.props.reduxState !== prevProps.reduxState){
-      this.setState({
-        caption: this.props.reduxState.caption,
-        id: this.props.reduxState.id
-      });
-    }
-  }
+  }, [image]);
 
   // Dispatch caption updates to saga
-  dispatchCaption = () => {
-    this.props.dispatch({type: `UPDATE_CAPTION`, payload: {caption: this.state.caption, id: this.state.id}});
-  }
-
-  // Set caption updates to local state
-  handleChange = (e) => {
-    this.setState({caption: e.target.value});
-  }
+  const dispatchCaption = () => dispatch({type: `UPDATE_CAPTION`, payload: {caption: caption, id: id}});
 
   // Permanently deletes this image
-  handleDelete = () => {
+  const handleDelete = () => {
     let popup = window.confirm(`Delete this image?`);
     if(popup){
-      this.props.dispatch({type: `DELETE_IMAGE`, payload: this.state.id});
-      this.pushHistory();
+      dispatch({type: `DELETE_IMAGE`, payload: id});
+      pushHistory();
     }
   }
 
-  // Saves changes to database, returns user to their profile
-  handleSave = () => {
-    this.dispatchCaption();
-    this.pushHistory();
+  // Save changes to database, returns user to their profile
+  const handleSave = () => {
+    dispatchCaption();
+    pushHistory();
   }
 
   // View user's profile
-  pushHistory = () => {
-    this.props.history.push('/profile');
-  }
+  const pushHistory = () => history.push('/profile');
 
-  render(){
-    const { classes } = this.props;
-
-    return(
-      <>
-        <center>
-          <div>
-            <Fab 
-              color="secondary" 
-              aria-label="edit" 
-              onClick={this.handleDelete}
-              style={{position:"relative",top:"30px",left:"147px"}}
-            >
-              <DeleteForeverIcon />
-            </Fab>
-            <div>
-            <img className="img" src={this.props.reduxState.image_url} alt={this.props.reduxState.description} />
-            </div>
-          </div>
-          </center>
-        <div className="edit-center-input">
-          <TextField 
-            id="outlined-basic" 
-            label="update caption" 
-            variant="outlined" 
-            value={this.state.caption || ''}
-            onChange={this.handleChange}
-            multiline 
-            InputLabelProps={{
-              classes: {
-                root: classes.cssLabel,
-                focused: classes.cssFocused,
-              }
-            }}
-            InputProps={{
-              classes: {
-                root: classes.cssOutlinedInput,
-                focused: classes.cssFocused,
-                notchedOutline: classes.notchedOutline,
-              }
-            }}
-            style={{width:"80%",marginBottom:"20px"}} 
-          />
-          <Button 
-            variant="contained" 
-            color="primary"
-            type="submit"
-            value="Save"
-            onClick={this.handleSave}
-            style={{width:"80%",marginBottom:"50px",backgroundColor:"#bc75ff"}}
+  return(
+    <>
+      <center>
+        <div>
+          <Fab 
+            color="secondary" 
+            aria-label="edit" 
+            onClick={handleDelete}
+            style={{position:"relative",top:"30px",left:"147px"}}
           >
-            Save
-          </Button>
+            <DeleteForeverIcon />
+          </Fab>
+          <div>
+          <img className="img" src={image.image_url} alt={image.description} />
+          </div>
         </div>
-        <NavBar />
-      </>
-    );
-  }
+        </center>
+      <div className="edit-center-input">
+        <TextField 
+          id="outlined-basic" 
+          label="update caption" 
+          variant="outlined" 
+          value={caption || ''}
+          onChange={(e)=>setCaption(e.target.value)}
+          multiline 
+          style={{width:"80%",marginBottom:"20px"}} 
+        />
+        <Button 
+          variant="contained" 
+          color="primary"
+          type="submit"
+          value="Save"
+          onClick={handleSave}
+          style={{width:"80%",marginBottom:"50px",backgroundColor:"#bc75ff"}}
+        >
+          Save
+        </Button>
+      </div>
+      <NavBar />
+    </>
+  );
 }
-
-const putReduxStateOnProps = (reduxState)=>({
-  reduxState: reduxState.image.thisImage
-});
-
-EditThisImage.propTypes = {
-  classes: PropTypes.object.isRequired,
-};
-
-export default withStyles(styles)(connect(putReduxStateOnProps)(EditThisImage));
