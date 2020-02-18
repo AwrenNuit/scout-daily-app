e405247ef13d039f38f6715bfbe661b4b377dff2
AwrenNuit <a.rennaker@yahoo.com>
2020-02-18 08:17:09 -0600

    converting component to use hooks

--- src/components/ViewThisImage/ViewThisImage.js
+++ src/components/ViewThisImage/ViewThisImage.js
@@ -1,97 +1,76 @@
-import React, {Component} from 'react';
-import {connect} from 'react-redux';
-import { Link } from "react-router-dom";
+import React, { useState, useEffect, useCallback } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { Link, useParams } from "react-router-dom";
 import './ViewThisImage.css';
 import FavoriteIcon from '@material-ui/icons/Favorite';
 import FavoriteBorderIcon from '@material-ui/icons/FavoriteBorder';
 import NavBar from '../NavBar/NavBar';
 import Comment from '../Comment/Comment';
 
-class ViewThisImage extends Component{
+export default function ViewThisImage() {
 
-  state = {
-    like: false,
-  }
+  const dispatch = useCallback(useDispatch());
+  const match = useParams();
+  const image = useSelector(state => state.viewThisImage);
+  const imageLike = useSelector(state => state.like);
+  const details = useSelector(state => state.user);
+  const [like, setLike] = useState(false);
 
-  componentDidMount(){
-    this.props.dispatch({type: `VIEW_THIS_IMAGE`, payload: this.props.match.params.id});
-    this.props.dispatch({type: `GET_LIKE`, payload: this.props.match.params.id});
-    
-  }
+  // Run on component mount
+  useEffect(()=>{
+    dispatch({type: `VIEW_THIS_IMAGE`, payload: match.id});
+    dispatch({type: `GET_LIKE`, payload: match.id});
+  }, [dispatch]);
 
-  componentDidUpdate(prevProps){
-    if(this.props.like !== prevProps.like){
-      if(this.props.like.liked !== true){
-        this.setState({
-          like: false,
-        });
-      }
-      else{
-        this.setState({
-          like: true,
-        });
-      }
+  // Run on like change
+  useEffect(()=>{
+    if(imageLike.like !== true){
+      setLike(false);
     }
-  }
+    else {
+      setLike(true);
+    }
+  }, [like]);
 
   // Dispatch like to saga
-  handleLike = (image) => {
-    if(this.state.like === false){
-      this.props.dispatch({type: `ADD_LIKE`, payload: image});
-    }
-    else{
-      this.props.dispatch({type: `SUB_LIKE`, payload: image});
-    }
-  }
+  const handleLike = image => !like ? dispatch({type: `ADD_LIKE`, payload: image}) : dispatch({type: `SUB_LIKE`, payload: image});
 
-  render(){
-    const details = this.props.reduxState;
-
-    return(
-      <>
-        <div className="view-card">
+  return(
+    <>
+      <div className="view-card">
+        <div>
+          <Link to={"/profile/"+details.user_id}>
+            <img className="view-avatar" src={details.avatar} alt={details.username} />
+            <span className="view-username">{details.username}</span>
+          </Link>
+        </div>
+        <span>
+          <center>
+            <img className="view-img" src={details.image_url} alt={details.caption} />
+          </center>
           <div>
-            <Link to={"/profile/"+details.user_id}>
-              <img className="view-avatar" src={details.avatar} alt={details.username} />
-              <span className="view-username">{details.username}</span>
-            </Link>
+            {like ?
+              <FavoriteIcon 
+                onClick={()=>handleLike(details.id)} 
+                style={{marginLeft:"40px",cursor:"pointer",color:"#b50000"}}
+              />
+              :
+              <FavoriteBorderIcon 
+                onClick={()=>handleLike(details.id)} 
+                style={{marginLeft:"40px",cursor:"pointer"}}
+              />
+            }
+            <span className="view-likes">{details.likes} likes</span>
           </div>
-          <span>
-            <center>
-              <img className="view-img" src={details.image_url} alt={details.caption} />
-            </center>
-            <div>
-              {this.state.like ?
-                <FavoriteIcon 
-                  onClick={()=>this.handleLike(details.id)} 
-                  style={{marginLeft:"40px",cursor:"pointer",color:"#b50000"}}
-                />
-                :
-                <FavoriteBorderIcon 
-                  onClick={()=>this.handleLike(details.id)} 
-                  style={{marginLeft:"40px",cursor:"pointer"}}
-                />
-              }
-              <span className="view-likes">{details.likes} likes</span>
-            </div>
-            <div className="view-caption">{details.caption}</div>
-          </span>
-        </div>
-        <div className="comment-div">
-          <hr className="view-hr" />
-          <Comment params_id={this.props.match.params.id} />
-        </div>
-        <div className="bottom-whitespace"></div>
-        <NavBar />
-      </>
-    );
-  }
+          <div className="view-caption">{details.caption}</div>
+        </span>
+      </div>
+      <div className="comment-div">
+        <hr className="view-hr" />
+        <Comment params_id={match.id} />
+      </div>
+      <div className="bottom-whitespace"></div>
+      <NavBar />
+    </>
+  );
 }
-
-const putReduxStateOnProps = (reduxState)=>({
-  reduxState: reduxState.image.viewThisImage,
-  user: reduxState.user,
-  like: reduxState.like
-});
-
-export default connect(putReduxStateOnProps)(ViewThisImage);
